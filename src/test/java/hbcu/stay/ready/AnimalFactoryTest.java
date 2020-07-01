c90d403c2d26f7796df825d2cfd0978e0322c817
Brent Oliver <brentoliver@Brents-MBP.fios-router.home>
2020-07-01 00:38:29 -0400

    Finished Lab 3 ready for review

--- src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
+++ src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
@@ -1,7 +1,52 @@
 package hbcu.stay.ready;
 
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import hbcu.stay.ready.animals.Cat;
+import hbcu.stay.ready.animals.Dog;
+import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
 
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void createDogTest() {
+        // Given
+        String givenName = "Brian";
+        Date givenBDate = new Date();
+        Dog testDog;
+
+        // When
+        testDog = AnimalFactory.createDog(givenName, givenBDate);
+
+        String retrievedName = testDog.getName();
+        Date retriveBDate = testDog.getBirthDate();
+
+        // Then
+        Assert.assertEquals(givenName, retrievedName);
+        Assert.assertEquals(givenBDate, retriveBDate);
+    }
+
+    @Test
+    public void createCatTest() {
+        // Given
+        String givenName = "Garfield";
+        Date givenBDate = new Date();
+        Cat testCat;
+
+        // When
+        testCat = AnimalFactory.createCat(givenName, givenBDate);
+
+        String retrievedName = testCat.getName();
+        Date retriveBDate = testCat.getBirthDate();
+
+        // Then
+        Assert.assertEquals(givenName, retrievedName);
+        Assert.assertEquals(givenBDate, retriveBDate);
+    }
+
 }
--- src/test/java/hbcu/stay/ready/DogHouseTest.java
+++ src/test/java/hbcu/stay/ready/DogHouseTest.java
@@ -1,6 +1,9 @@
 package hbcu.stay.ready;
 
 import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import hbcu.stay.ready.animals.Dog;
 import hbcu.stay.ready.animals.animal_storage.DogHouse;
@@ -9,11 +12,88 @@ import java.util.Date;
 
 
 public class DogHouseTest {
+    
+    Dog dog;
+    @Before
+    public void initiliaze() {
+        this.dog = new Dog(null, null, 25);
+        DogHouse.clear();
+    }
+    
     // TODO - Create tests for `void add(Dog dog)`
+    @Test
+    public void addTest() {
+        // Given
+        Dog expectedDog;
+
+        // When
+        DogHouse.add(dog);
+        expectedDog = DogHouse.getDogById(25);
+
+        // Then
+        Assert.assertEquals(expectedDog, dog);
+    }
+
     // TODO - Create tests for `void remove(Integer id)`
+    @Test
+    public void removeIdTest() {
+        // Given
+        Dog expectedDog = null;
+
+        // When
+        DogHouse.add(dog);
+        DogHouse.remove(25);
+
+        Dog actualDog = DogHouse.getDogById(25);
+
+        // Then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
+
     // TODO - Create tests for `void remove(Dog dog)`
+    @Test
+    public void removeDogTest() {
+        // Given
+        Dog expectedDog = null;
+
+        // When
+        DogHouse.add(dog);
+        DogHouse.remove(dog);
+
+        Dog actualDog = DogHouse.getDogById(25);
+
+        // Then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
+
     // TODO - Create tests for `Dog getDogById(Integer id)`
+    @Test
+    public void getDogByIdTest() {
+        // Given
+        Dog expectedDog = dog;
+
+        // When
+        DogHouse.add(expectedDog);
+        Dog actualDog = DogHouse.getDogById(25);
+
+        // Then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
+
     // TODO - Create tests for `Integer getNumberOfDogs()`
+    @Test
+    public void getNumberOfDogsTest() {
+        // Given
+        int expectedNumOfDogs = 1;
+
+        // When
+        DogHouse.add(dog);
+        int actualNumOfDogs = DogHouse.getNumberOfDogs();
+
+        // Then
+        Assert.assertEquals(expectedNumOfDogs, actualNumOfDogs);
+    }
+
 
     @Test
     public void testGetNumberOfDogs() {
