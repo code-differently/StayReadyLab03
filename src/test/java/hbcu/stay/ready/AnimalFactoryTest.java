37d1f246a44d434bf4633c6b87dfbc5c55e6b54f
johndacosmo <jpali@bellsouth.net>
2020-07-02 03:04:13 -0400

    Lab ready for review

--- src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
+++ src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
@@ -1,7 +1,48 @@
 package hbcu.stay.ready;
 
+import org.junit.Assert;
+
+import hbcu.stay.ready.animals.Animal;
+import hbcu.stay.ready.animals.Dog;
+import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
+import hbcu.stay.ready.animals.Cat;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.junit.Test;
 
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+
+@Test
+public void createCatTest(){
+
+    // Given 
+    String name = "mil";
+    Date birthDate = new Date();
+    Cat animal = AnimalFactory.createCat(name, birthDate);
+    
+    assertEquals(name, animal.getName());
+    assertEquals(birthDate, animal.getBirthDate());
+
+}
+
+@Test
+public void createDogTest(){
+
+    // Given 
+    String name = "Milo";
+    Date birthDate = new Date();
+    Dog animal = AnimalFactory.createDog(name, birthDate);
+    
+    assertEquals(name, animal.getName());
+    assertEquals(birthDate, animal.getBirthDate());
+
+}
+
+
 }
