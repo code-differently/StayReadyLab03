a3a9985032fe00cf77a71c31eb120a79e0fb5560
Aldo <aalmeidarojo18@students.desu.edu>
2020-07-03 14:45:34 -0400

    Tests completed

--- src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
+++ src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
@@ -1,7 +1,49 @@
 package hbcu.stay.ready;
 
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import hbcu.stay.ready.animals.Dog;
+import hbcu.stay.ready.animals.Cat;
+import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
 
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void creatDogTest(){
+        // Given
+        Date date= new Date();
+        String name= "Bill";
+
+        // When
+        Dog dog = AnimalFactory.createDog("Bill", date);
+        String actualName=dog.getName();
+        Date actualDate=dog.getBirthDate();
+
+        // Then
+        Assert.assertEquals(name, actualName);
+        Assert.assertEquals(date, actualDate);
+
+    }
+
+    @Test
+    public void creatCatTest(){
+        // Given
+        Date date= new Date();
+        String name= "Bill";
+
+        // When
+        Cat cat = AnimalFactory.createCat("Bill", date);
+        String actualName=cat.getName();
+        Date actualDate=cat.getBirthDate();
+
+        // Then
+        Assert.assertEquals(name, actualName);
+        Assert.assertEquals(date, actualDate);
+
+    }
 }
--- src/test/java/hbcu/stay/ready/CatTest.java
+++ src/test/java/hbcu/stay/ready/CatTest.java
@@ -1,14 +1,16 @@
 package hbcu.stay.ready;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import hbcu.stay.ready.animals.Cat;
 
 import java.util.Date;
+import hbcu.stay.ready.animals.Animal;
+import hbcu.stay.ready.animals.Mammal;
 
 
 public class CatTest {
-    // TODO - Create tests for `void setName(String name)`
     // TODO - Create tests for `speak`
     // TODO - Create tests for `setBirthDate(Date birthDate)`
     // TODO - Create tests for `void eat(Food food)`
@@ -16,6 +18,17 @@ public class CatTest {
     // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
     // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
 
+    String givenName;
+    Date givenBirthDate;
+    Integer givenId;
+    Cat cat;
+    @Before
+    public void setUp(){
+        givenName="Alberto";
+        givenBirthDate= new Date();
+        givenId=11;
+        cat=new Cat(givenName, givenBirthDate, givenId);
+    }
 
     @Test
     public void constructorTest() {
@@ -38,4 +51,86 @@ public class CatTest {
         Assert.assertEquals(givenId, retrievedId);
     }
 
+    @Test
+    public void setNameTest(){
+        // Given
+        String expectedName="Jo";
+        // When
+        cat.setName("Jo");
+        String actualName=cat.getName();
+        // Then
+        Assert.assertEquals(expectedName, actualName);
+
+    }
+
+    @Test
+    public void speakTest(){
+        //Given
+        String expected="meow!";
+
+        // When
+        String actual=cat.speak();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setBirthDate(){
+        // Given
+        Date expected = new Date();
+
+        // When
+        cat.setBirthDate(expected);
+        Date actual=cat.getBirthDate();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void eatTest(){
+        // Given
+        Food apple= new Food();
+        int expected = 1;
+
+        // When
+        cat.eat(apple);
+        int actual = cat.getNumberOfMealsEaten();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getIDTest(){
+        // Given
+        int expected = 11;
+
+        //When
+        int actual=cat.getId();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void animalTest(){
+        // Given
+        boolean expected= true;
+        // When
+        boolean actual= cat instanceof Animal;
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mammalTest(){
+        // Given
+        boolean expected= true;
+        // When
+        boolean actual= cat instanceof Mammal;
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
