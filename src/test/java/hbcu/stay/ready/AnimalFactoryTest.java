ae8eaf8421364029d5c2fa8f888156cafc94c75e
KeseanaH <45104070+KeseanaH@users.noreply.github.com>
2020-07-03 22:10:57 -0400

    completed lab 03, ready for review

--- src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
+++ src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
@@ -1,7 +1,27 @@
 package hbcu.stay.ready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import hbcu.stay.ready.animals.Cat;
+import hbcu.stay.ready.animals.Dog;
+import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
 
 public class AnimalFactoryTest {
-    //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
-    //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void creatCatTest(){
+
+        Cat cat = AnimalFactory.createCat("Qwazi", null);
+
+        Assert.assertNotNull(cat);
+    }
+
+    @Test
+    public void createDogTest(){
+
+        Dog dog = AnimalFactory.createDog("Snow", null);
+        
+        Assert.assertNotNull(dog);
+    }
 }
--- src/test/java/hbcu/stay/ready/DogTest.java
+++ src/test/java/hbcu/stay/ready/DogTest.java
@@ -2,17 +2,15 @@ package hbcu.stay.ready;
 
 import org.junit.Assert;
 import org.junit.Test;
-import hbcu.stay.ready.animals.Dog;
 
+import hbcu.stay.ready.animals.Animal;
+import hbcu.stay.ready.animals.Dog;
+import hbcu.stay.ready.animals.Mammal;
+
+import java.util.Date;
 
 public class DogTest {
-    // TODO - Create tests for `new Dog(String name, Date birthDate, Integer id)`
-    // TODO - Create tests for `speak`
-    // TODO - Create tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
-    // TODO - Create tests for `Integer getId()`
-    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
-    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+
     @Test
     public void setNameTest() {
         // Given (a name exists and a dog exists)
@@ -25,5 +23,62 @@ public class DogTest {
         // Then (we expect to get the given name from the dog)
         String dogName = dog.getName();
         Assert.assertEquals(dogName, givenName);
+    }
+
+    @Test
+    public void setBirthDateTest(){
+        Dog dog = new Dog("Jacks", null, 01);
+
+        dog.setBirthDate(new Date());
+        Date actual = dog.getBirthDate();
+        Date expected = new Date();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void speakTest(){
+        Dog dog = new Dog("Koda", null, 01);
+
+        String actual = dog.speak();
+        String expected = "bark!";
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void eatTest(){
+        Dog dog = new Dog("puppy", new Date(), 02);
+        Food dogFood = new Food();
+
+        dog.eat(dogFood);
+        int actual = dog.getNumberOfMealsEaten();
+        int expected = 1;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getIdTest(){
+        Dog dog = new Dog("Koda", null, 03);
+
+        Integer actual = dog.getId();
+        Integer expected = 3;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkAnimalInheritance(){
+        Dog dog = new Dog("Koda", null, 01);
+
+        Assert.assertTrue(dog instanceof Animal);
+    }
+
+    @Test
+    public void checkMammalInheritance(){
+        Dog dog = new Dog("Koda", null, 01);
+
+        Assert.assertTrue(dog instanceof Mammal);
     }
 }
