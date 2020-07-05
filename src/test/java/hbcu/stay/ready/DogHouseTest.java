4030e5bcdb5bfbc6d8eaed64b99548b12dd490bc
SajidMA <smahmed@aggies.ncat.edu>
2020-07-05 03:38:45 -0400

    Added Lab 03 ready for review

--- src/test/java/hbcu/stay/ready/DogHouseTest.java
+++ src/test/java/hbcu/stay/ready/DogHouseTest.java
@@ -1,32 +1,124 @@
 package hbcu.stay.ready;
 
-import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
+import org.junit.Assert;
 import org.junit.Test;
+
 import hbcu.stay.ready.animals.Dog;
 import hbcu.stay.ready.animals.animal_storage.DogHouse;
 
-import java.util.Date;
-
-
 public class DogHouseTest {
-    // TODO - Create tests for `void add(Dog dog)`
-    // TODO - Create tests for `void remove(Integer id)`
-    // TODO - Create tests for `void remove(Dog dog)`
-    // TODO - Create tests for `Dog getDogById(Integer id)`
-    // TODO - Create tests for `Integer getNumberOfDogs()`
-
+    
     @Test
-    public void testGetNumberOfDogs() {
-        // Given (some
-        String name = "Milo";
-        Date birthDate = new Date();
-        Dog animal = AnimalFactory.createDog(name, birthDate);
+    public void addTest() {
+        // Given
+        Dog d = new Dog(null, null, null);
         DogHouse.clear();
 
         // When
-        DogHouse.add(animal);
+        DogHouse.add(d);
+        int expected = 1;
+        int actual = DogHouse.getNumberOfDogs();
 
         // Then
-        DogHouse.getNumberOfDogs();
+        Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void removeByDogTest() {
+        // Given
+        Dog d1 = new Dog(null, null, null);
+        Dog d2 = new Dog(null, null, null);
+        Dog d3 = new Dog(null, null, null);
+        DogHouse.clear();
+
+        // When
+        DogHouse.add(d1);
+        DogHouse.add(d2);
+        DogHouse.add(d3);
+        DogHouse.remove(d1);
+        int expected = 2;
+        int actual = DogHouse.getNumberOfDogs();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeByIdTest() {
+        // Given
+        Dog d1 = new Dog(null, null, 10);
+        Dog d2 = new Dog(null, null, 20);
+        Dog d3 = new Dog(null, null, 30);
+        DogHouse.clear();
+
+        // When
+        DogHouse.add(d1);
+        DogHouse.add(d2);
+        DogHouse.add(d3);
+        DogHouse.remove(10);
+        int expected = 2;
+        int actual = DogHouse.getNumberOfDogs();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getDogByIdTest() {
+        // Given
+        Dog d1 = new Dog(null, null, 10);
+        Dog d2 = new Dog(null, null, 20);
+        Dog d3 = new Dog(null, null, 30);
+        DogHouse.clear();
+
+        // When
+        DogHouse.add(d1);
+        DogHouse.add(d2);
+        DogHouse.add(d3);
+        Dog expected = d2;
+        Dog actual = DogHouse.getDogById(20);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNumberOfDogsTest() {
+        // Given
+        Dog d1 = new Dog(null, null, null);
+        Dog d2 = new Dog(null, null, null);
+        Dog d3 = new Dog(null, null, null);
+        DogHouse.clear();
+
+        // When
+        DogHouse.add(d1);
+        DogHouse.add(d2);
+        DogHouse.add(d3);
+        int expected = 3;
+        int actual = DogHouse.getNumberOfDogs();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void clearTest() {
+        // Given
+        Dog d1 = new Dog(null, null, null);
+        Dog d2 = new Dog(null, null, null);
+        Dog d3 = new Dog(null, null, null);
+        DogHouse.clear();
+
+        // When
+        DogHouse.add(d1);
+        DogHouse.add(d2);
+        DogHouse.add(d3);
+        DogHouse.clear();
+        int expected = 0;
+        int actual = DogHouse.getNumberOfDogs();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
