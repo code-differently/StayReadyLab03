63108728ebac75c8bc8804fca85197bac40eb938
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-05 22:08:21 -0400

    Finished lab3

--- src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
+++ src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
@@ -1,7 +1,59 @@
 package hbcu.stay.ready;
 
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import hbcu.stay.ready.animals.Animal;
+import hbcu.stay.ready.animals.Cat;
+import hbcu.stay.ready.animals.Dog;
+import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
 
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
+    @Test
+    public void createDogTest(){
+        // Given
+        String name = "benji";
+        Date birthDate = new Date();
+        AnimalFactory dogFactory = new AnimalFactory();
+
+        // When 
+        Dog fido = dogFactory.createDog(name, birthDate);
+
+        String expectedName = "benji";
+        Date expectedDate = birthDate;
+
+        String actualName = fido.getName();
+        Date actualDate = fido.getBirthDate();
+
+        // Then
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedDate, actualDate);
+    
+    }
+
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    @Test
+    public void createCatTest(){
+        // Given
+        String name = "sylvester";
+        Date birthDate = new Date();
+        AnimalFactory catFactory = new AnimalFactory();
+
+        // When 
+        Cat feline = catFactory.createCat(name, birthDate);
+
+        String expectedName = "sylvester";
+        Date expectedDate = birthDate;
+
+        String actualName = feline.getName();
+        Date actualDate = feline.getBirthDate();
+
+        // Then
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedDate, actualDate);
+    
+    }
 }
--- src/test/java/hbcu/stay/ready/CatHouseTest.java
+++ src/test/java/hbcu/stay/ready/CatHouseTest.java
@@ -1,10 +1,93 @@
 package hbcu.stay.ready;
 
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import hbcu.stay.ready.animals.Cat;
+import hbcu.stay.ready.animals.animal_storage.CatHouse;
 
 public class CatHouseTest {
     // TODO - Create tests for `void add(Cat cat)`
+    @Test
+    public void addTest(){
+        // Given
+        CatHouse catHouse = new CatHouse();
+        Cat tom = new Cat("tom", new Date(), 1);
+
+        // When
+        catHouse.add(tom);
+        Integer expected = 1;
+        Integer actual = catHouse.getNumberOfCats();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create tests for `void remove(Integer id)`
+    @Test
+    public void removeByIdTest(){
+        // Given
+        CatHouse catHouse = new CatHouse();
+        Cat tom = new Cat("tom", new Date(), 1);
+
+        // When
+        catHouse.add(tom);
+        catHouse.remove(1);
+        Integer expected = 0;
+        Integer actual = catHouse.getNumberOfCats();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create tests for `void remove(Cat cat)`
+    @Test
+    public void removeByCatTest(){
+        // Given
+        CatHouse catHouse = new CatHouse();
+        Cat tom = new Cat("tom", new Date(), 1);
+
+        // When
+        catHouse.add(tom);
+        catHouse.remove(tom);
+        Integer expected = 0;
+        Integer actual = catHouse.getNumberOfCats();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create tests for `Cat getCatById(Integer id)`
+    @Test
+    public void getCatByIdTest(){
+        // Given
+        CatHouse catHouse = new CatHouse();
+        Cat tom = new Cat("tom", new Date(), 1);
+
+        // When
+        catHouse.add(tom);
+        Cat expected = tom;
+        Cat actual = catHouse.getCatById(1);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create tests for `Integer getNumberOfCats()`
+    @Test
+    public void getNumberOfCatsTest(){
+        // Given
+        CatHouse catHouse = new CatHouse();
+        Cat tom = new Cat("tom", new Date(), 1);
+
+        // When
+        catHouse.add(tom);
+        Integer expected = 1;
+        Integer actual = catHouse.getNumberOfCats();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
