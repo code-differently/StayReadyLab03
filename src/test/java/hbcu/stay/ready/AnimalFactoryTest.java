38260f90b1a3d15a513dcb86a5343e5f2b9475e5
Angela Session <asession001@gmail.com>
2020-07-05 21:52:28 -0400

    Test Complete

--- src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
+++ src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
@@ -1,7 +1,44 @@
 package hbcu.stay.ready;
 
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import hbcu.stay.ready.animals.Cat;
+import hbcu.stay.ready.animals.Dog;
+import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
 
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void creatDogTest(){
+
+        String name = "Cody";
+        Date birthDate = new Date();
+        Dog dog = AnimalFactory.createDog(name, birthDate);
+
+        String actualName = dog.getName();
+        Date actualDate = dog.getBirthDate();
+
+        Assert.assertEquals(name, actualName);
+        Assert.assertEquals(birthDate, actualDate);  
+    }
+
+
+    @Test
+    public void createCatTest(){
+
+        String name = "Zack";
+        Date birthDate = new Date();
+        Cat cat = AnimalFactory.createCat(name, birthDate);
+
+        String actualName = cat.getName();
+        Date actualDate = cat.getBirthDate();
+
+        Assert.assertEquals(name, actualName);
+        Assert.assertEquals(birthDate, actualDate);
+    }
 }
